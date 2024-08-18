678e11704868db7dcebe87be0bcccfe7f6852c10
jackson.gao <jackson.gao@atomintl.com>
2024-08-18 22:08:33 +0800

    feat: md简写说明修改

--- packages/simple-ansi/README.md
+++ packages/simple-ansi/README.md
@@ -1,12 +1,16 @@
-# 简单ansi
-代码只有1kb，非常可靠，支持常用字体颜色，背景色，样式的选择。
+# 简单 ANSI
+代码只有大概1kb，使用非常简单可靠，支持常用字体颜色（8+8种），背景色（8+8种），样式（8种）的选择。
 
 ## 安装
 pnpm add simple-ansi
 
 ## 使用
 ```javascript
-import simpleAnsi, { color } from 'simple-ansi'
+import simpleAnsi, { color, bgColor, style } from 'simple-ansi'
 
-console.log(simpleAnsi('欢迎使用 mini-chalk', [color.red]))
+/**
+ * 举个🌰：比如打印一个 白色字体，红色背景色，斜体的 log
+*/
+console.log(simpleAnsi('欢迎使用 mini-chalk', [color.white, bgColor.red, style.italics]))
+
 ```
--- packages/simple-ansi/src/index.js
+++ packages/simple-ansi/src/index.js
@@ -1,16 +1,12 @@
-/**
- * 设置文本颜色
-*/
 export const color = {
-  black: '\x1b[30m', // 黑色
-  red: '\x1b[31m', // 红色
-  green: '\x1b[32m', // 绿色
-  yellow: '\x1b[33m', // 黄色
-  blue: '\x1b[34m', // 蓝色
-  magenta: '\x1b[35m', // 品红 (Magenta)
-  cyan: '\x1b[36m', // 青色 (Cyan)
-  white: '\x1b[37m', // 白色
-  // 30 + 60
+  black: '\x1b[30m',
+  red: '\x1b[31m',
+  green: '\x1b[32m',
+  yellow: '\x1b[33m',
+  blue: '\x1b[34m',
+  magenta: '\x1b[35m',
+  cyan: '\x1b[36m',
+  white: '\x1b[37m',
   brightBlack: '\x1b[90m',
   brightRed: '\x1b[91m',
   brightGreen: '\x1b[92m',
@@ -21,19 +17,15 @@ export const color = {
   brightWhite: '\x1b[97m'
 }
 
-/**
- * 设置文本背景色
-*/
 export const bgColor = {
-  black: '\x1b[40m', // 黑色
-  red: '\x1b[41m', // 红色
-  green: '\x1b[42m', // 绿色
-  yellow: '\x1b[43m', // 黄色
-  blue: '\x1b[44m', // 蓝色
-  magenta: '\x1b[45m', // 洋红色 (Magenta)
-  cyan: '\x1b[46m', // 青色 (Cyan)
-  white: '\x1b[47m', // 白色
-  // 40 + 60
+  black: '\x1b[40m',
+  red: '\x1b[41m',
+  green: '\x1b[42m',
+  yellow: '\x1b[43m',
+  blue: '\x1b[44m',
+  magenta: '\x1b[45m',
+  cyan: '\x1b[46m',
+  white: '\x1b[47m',
   brightBlack: '\x1b[100m',
   brightRed: '\x1b[101m',
   brightGreen: '\x1b[102m',
@@ -44,18 +36,15 @@ export const bgColor = {
   brightWhite: '\x1b[107m'
 }
 
-/**
- * 设置文本样式
-*/
 export const style = {
-  bold: '\x1b[1m', // 粗体/高亮（Bold/Bright）
-  dim: '\x1b[2m', // 淡色/暗淡（Dim/Faint）
-  italics: '\x1b[3m', // 斜体（Italic）（有些终端不支持）
-  underline: '\x1b[4m', // 下划线（Underline）
-  blink: '\x1b[5m', // 闪烁（Blink）（有些终端不支持或被禁用）
-  reverse: '\x1b[7m', // 反显（Reverse/Inverse，背景色和前景色互换）
-  hidden: '\x1b[8m', // 隐藏（Hidden）（有些终端不支持）
-  strikethrough: '\x1b[9m' // 删除线（Strikethrough）（有些终端不支持）
+  bold: '\x1b[1m',
+  dim: '\x1b[2m',
+  italics: '\x1b[3m',
+  underline: '\x1b[4m',
+  blink: '\x1b[5m',
+  reverse: '\x1b[7m',
+  hidden: '\x1b[8m',
+  strikethrough: '\x1b[9m'
 }
 
 const simpleAnsi = (text, options = []) => {
